cf1d447913bebdd0c91aa5474c1ec72cc85f9ee1
Nicolás Tasso <ntasso@srk.com.ar>
2024-12-17 00:13:48 -0300

    update

--- utils/plot_functions.py
+++ utils/plot_functions.py
@@ -212,7 +212,7 @@ def plot_q_vs_p(ax, data, params, stop_idx, xmin='auto', xmax='auto', ymin='auto
 
     # Set axis labels
     ax.set_xlabel('Mid stress, $p^{*} = \dfrac{\sigma_{x}+\sigma_{y}}{2}$ [kPa]')
-    ax.set_ylabel('Deviatoric stress, $q^{*} = \\sqrt{2} \\cdot |\\sigma-pI|$ [kPa]')
+    ax.set_ylabel('Dev. stress, $q^{*} = \\sqrt{2} \\cdot |\\sigma-pI|$ [kPa]')
     
 
     # Add grid lines for better visualization
@@ -255,28 +255,30 @@ def plot_q_vs_p(ax, data, params, stop_idx, xmin='auto', xmax='auto', ymin='auto
 
     ax.legend(loc='upper left')
 
-
-
-def plot_e_vs_logp(ax, data, params, stop_idx, xmin='auto', xmax='auto', ymin='auto', ymax='auto',log_scale=True):
+def plot_e_vs_logp(ax, data, params, stop_idx, xmin='auto', xmax='auto', ymin='auto', ymax='auto', log_scale=True):
     """
-    Plots back stress ratio (\u03b1) versus the number of uniform cycles (N) on the provided Matplotlib Axes.
+    Plots the void ratio (e) versus the logarithm of mean effective stress (p*) on the provided Matplotlib Axes.
 
     Parameters:
     - ax: Matplotlib Axes
         The Axes object where the plot will be drawn.
     - data: pandas.DataFrame
         A DataFrame containing the data to be plotted. Must include the following columns:
-        'N', 'alphaxx', 'alphayy', and 'alphaxy'.
+        'p_ast' (mean effective stress), 'e' (void ratio), 'Gamma', and 'xi'.
+    - params: dict
+        A dictionary of parameters, including 'lambda', which represents the slope of the critical state line in log(p)-e space.
     - stop_idx: int
         The index in the data up to which the plot should be drawn.
     - xmin: float or 'auto', optional
-        Minimum x-axis limit. If 'auto', it is calculated based on the data in the 'N' column.
+        Minimum x-axis limit. If 'auto', it is calculated based on the data in the 'p_ast' column.
     - xmax: float or 'auto', optional
-        Maximum x-axis limit. If 'auto', it is calculated based on the data in the 'N' column.
+        Maximum x-axis limit. If 'auto', it is calculated based on the data in the 'p_ast' column.
     - ymin: float or 'auto', optional
-        Minimum y-axis limit. If 'auto', it is calculated based on the data in the columns 'alphaxx', 'alphayy', and 'alphaxy'.
+        Minimum y-axis limit. If 'auto', it is calculated based on the data in the 'e' column.
     - ymax: float or 'auto', optional
-        Maximum y-axis limit. If 'auto', it is calculated based on the data in the columns 'alphaxx', 'alphayy', and 'alphaxy'.
+        Maximum y-axis limit. If 'auto', it is calculated based on the data in the 'e' column.
+    - log_scale: bool, optional
+        Whether to use a logarithmic scale for the x-axis. Default is True.
 
     Returns:
     - None
@@ -291,36 +293,37 @@ def plot_e_vs_logp(ax, data, params, stop_idx, xmin='auto', xmax='auto', ymin='a
     ax.grid(color='gray', alpha=0.2)
 
     # Helper function to calculate axis limits
-    xmin, xmax = set_limit(xmin, xmax, data, ['p_ast'])  # Set x-axis limits based on the 'N' column
-    ymin, ymax = set_limit(ymin, ymax, data, ['e'])  # Set y-axis limits based on the back stress ratio columns
-
+    xmin, xmax = set_limit(xmin, xmax, data, ['p_ast'])  # Set x-axis limits based on the 'p_ast' column
+    ymin, ymax = set_limit(ymin, ymax, data, ['e'])  # Set y-axis limits based on the 'e' column
 
     # Apply the calculated axis limits
     ax.set_xlim(xmin, xmax)
     ax.set_ylim(ymin, ymax)
 
-    if log_scale == True:
+    if log_scale:
         ax.set_xscale('log')
-    
-    ax.plot([1,100000],[data['Gamma'].values[stop_idx-1],
-                         data['Gamma'].values[-1]-params['lambda'].values[0]*np.log(100000)],
-                         color='r',label='Critical state')
-    
 
-    ax.plot(data['p_ast'].values[:stop_idx],data['e'].values[:stop_idx],color='k')
-    ax.scatter(data['p_ast'].values[stop_idx-1],data['e'].values[stop_idx-1],color='k')
-    
+    # Plot the critical state line
+    ax.plot([1, 100000], [data['Gamma'].values[stop_idx-1],
+                          data['Gamma'].values[-1] - params['lambda'].values[0] * np.log(100000)],
+            color='r', label='Critical state')
 
-    if data['xi'].values[stop_idx-1]>0:
+    # Plot the data points and trajectory
+    ax.plot(data['p_ast'].values[:stop_idx], data['e'].values[:stop_idx], color='k')
+    ax.scatter(data['p_ast'].values[stop_idx-1], data['e'].values[stop_idx-1], color='k')
+
+    # Determine the color for the state line based on 'xi'
+    if data['xi'].values[stop_idx-1] > 0:
         colorstate = 'limegreen'
     else:
-        colorstate ='firebrick'
+        colorstate = 'firebrick'
 
+    # Plot the state line
     p_ast_value = data['p_ast'].values[stop_idx-1]
     e_value = data['e'].values[stop_idx-1]
-    ecs_value = data['e'].values[stop_idx-1]-data['xi'].values[stop_idx-1]
-    ax.plot([p_ast_value,p_ast_value],[e_value,ecs_value],color=colorstate,zorder=1,
-             label='$\\xi =$ '+str(np.round(data['xi'].values[stop_idx-1],decimals=2)))
+    ecs_value = data['e'].values[stop_idx-1] - data['xi'].values[stop_idx-1]
+    ax.plot([p_ast_value, p_ast_value], [e_value, ecs_value], color=colorstate, zorder=1,
+            label='$\\xi =$ ' + str(np.round(data['xi'].values[stop_idx-1], decimals=2)))
 
+    # Add legend
     ax.legend(loc='lower left')
-
